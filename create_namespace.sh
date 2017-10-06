6e3cd918f266fd1eb57cf495cf29489e0ec7dc14
Eduardo Patrocinio <epatro@gmail.com>
2017-10-06 18:36:10 -0400

    Deploy CAM

--- create_namespace.sh
+++ create_namespace.sh
@@ -1,3 +1,6 @@
+NAMESPACE=$1
+
+
 kubectl create namespace $NAMESPACE
 ./switch_namespace.sh $NAMESPACE
 
--- deploy_cam.sh
+++ deploy_cam.sh
@@ -1,5 +1,7 @@
 ./create_namespace.sh cam
 
+PROXY_IP=169.45.207.217
+
 function create_log_pv {
 	echo Creating log PV
 	kubectl delete pv d-10	
@@ -10,12 +12,19 @@ function create_log_pv {
 function create_db_pv {
 	echo Creating DB PV
 	kubectl delete pv d-11
+	kubectl delete pv cam-mongo-nfs
 	kubectl create -f cam_install/cam/pre-install/cam-mongo-pv.yaml 
 }
 
+function delete_log_pvc {
+	echo Deleting log PVC
+	kubectl delete pvc cam-logs-nfs
+}
+
+
 function create_log_pvc {
+	delete_log_pvc
 	echo Creating log PVC
-	kubectl delete pvc cam-log
 	kubectl create -f cam_config/cam_log_pvc.yaml	
 }
 
@@ -23,10 +32,21 @@ function deploy_create_log_dirs {
 	echo Creating log directories
 	cd cam_config
 	./deploy_create_log_dirs.sh
+	cd ..
+}
+
+function deploy_cam {
+	echo Deploing CAM
+	cd cam_install
+	helm delete --purge cam
+	helm install --name cam --set host.ip=$PROXY_IP --set license=accept cam
+	cd ..
 }
 
 #create_log_pv
 #create_db_pv
 #create_log_pvc
-deploy_create_log_dirs
+#deploy_create_log_dirs
+#delete_log_pvc
+deploy_cam
 
