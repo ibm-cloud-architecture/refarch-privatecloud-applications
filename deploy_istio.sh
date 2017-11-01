af6dfcd7be6f55f5d7c9d7868994b8a3477eca9c
Eduardo Patrocinio <epatro@gmail.com>
2017-11-01 12:56:52 -0400

    Update deploy istio

--- deploy_istio.sh
+++ deploy_istio.sh
@@ -1,2 +1,24 @@
-helm repo add incubator  http://storage.googleapis.com/kubernetes-charts-incubator
+./create_namespace.sh istio-system
 
+NAME=istio
+CHART=./istio-0.2.7-chart5.tgz
+
+function delete_istio {
+	helm delete --purge $NAME
+}
+
+function deploy_istio {
+	echo Downloading istio...
+	wget https://kubernetes-charts-incubator.storage.googleapis.com/istio-0.2.7-chart5.tgz
+
+	echo Deploying istio...
+	helm install --name $NAME $CHART
+
+}
+
+#delete_istio
+cd /tmp
+#deploy_instio
+
+echo Installing istio...
+helm upgrade istio $CHART --reuse-values --set istio.install=true
