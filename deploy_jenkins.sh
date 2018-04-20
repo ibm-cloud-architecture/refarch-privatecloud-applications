da3417d3e41fe1385656a7b507fad1e1b5ffd16f
Eduardo Patrocinio <epatro@gmail.com>
2018-04-20 14:57:26 -0400

    Fix deploy Jenkins

--- deploy_jenkins.sh
+++ deploy_jenkins.sh
@@ -2,12 +2,12 @@
 
 NAME=jenkins
 
-helm delete --purge $NAME
+helm delete --purge $NAME --tls
 
 helm install --name $NAME stable/jenkins -f jenkins_config/values.yaml
 
 SLAVE_NAME=slave
 
-helm delete --purge $SLAVE_NAME
-helm install --name $SLAVE_NAME ~/github/charts/stable/slave
+helm delete --purge $SLAVE_NAME --tls
+helm install --name $SLAVE_NAME ~/github/charts/stable/slave --tls
 
