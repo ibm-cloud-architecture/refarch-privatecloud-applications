d1a91870ff54514b79ff57e5db3f0e021a8ead14
Eduardo Patrocinio <epatro@gmail.com>
2018-07-05 13:11:42 -0300

    Deploy HTTP

---
+++ deploy_db2.sh
@@ -0,0 +1,13 @@
+SECRET="eyJuYS5jdW11bHVzcmVwby5jb20iOnsidXNlcm5hbWUiOiJ0b2tlbiIsInBhc3N3b3JkIjoiOTkxOTE5NGIwMDU3MDM0N2FmZTA1YmQzNjljN2Y3MmYiLCJlbWFpbCI6ImlkZWxpZGphQGNhLmlibS5jb20iLCJhdXRoIjoiZEc5clpXNDZPVGt4T1RFNU5HSXdNRFUzTURNME4yRm1aVEExWW1Rek5qbGpOMlkzTW1ZPSJ9fQ=="
+
+. ./helm.sh
+./create_namespace.sh db2
+
+helm repo add ibm https://raw.githubusercontent.com/ibm-cloud-architecture/charts/master/stable
+
+helm delete --purge db2 $HELM_OPTIONS
+
+
+helm install --name db2 \
+	--set image.secret=$SECRET,license=accept \
+	ibm/ibm-db2oltp-dev $HELM_OPTIONS
--- deploy_httpd.sh
+++ deploy_httpd.sh
@@ -1,7 +1,10 @@
+. ./helm.sh
+
 ./create_namespace.sh httpd
 
 echo Removing httpd...
-helm delete --purge httpd
+helm delete --purge httpd $HELM_OPTIONS
 
 echo Deploying httpd...
-helm install --name httpd --debug ibm/httpd
+helm install --name httpd --debug ibm/httpd $HELM_OPTIONS
+ 
